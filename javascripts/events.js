ae1429adefa5f057640fc4842962cc4d1366b518
Andy <amillion3@gmail.com>
2018-05-22 17:57:26 -0500

    updated navbar links

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,14 +1,15 @@
 const myLinks = () => {
-  $(document).click((e) => {
-    if (e.target.id === 'authenticate') {
+  $(document).click(e => {
+    console.log(e);
+    if (e.target.id === 'nav-auth') {
       $('#myMovies').addClass('hide');
       $('#search').addClass('hide');
       $('#authScreen').removeClass('hide');
-    } else if (e.target.id === 'mine') {
+    } else if (e.target.id === 'nav-my-movies') {
       $('#myMovies').removeClass('hide');
       $('#search').addClass('hide');
       $('#authScreen').addClass('hide');
-    } else if (e.target.id === 'navSearch') {
+    } else if (e.target.id === 'nav-search') {
       $('#myMovies').addClass('hide');
       $('#search').removeClass('hide');
       $('#authScreen').addClass('hide');
